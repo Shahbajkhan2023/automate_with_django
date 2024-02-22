fdf102379901f21dd046584199b1221294a3be3a
shahbaj <rk2403476@gmil.com>
2024-02-22 09:22:13 +0530

    immportdata from the frontend

--- dataentry/admin.py
+++ dataentry/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Student, Customer
+from .models import Student, Customer, Employee
 
 # Register your models here.
 
 admin.site.register(Student)
 admin.site.register(Customer)
+admin.site.register(Employee)
+
 
 
--- dataentry/management/commands/importdata.py
+++ dataentry/management/commands/importdata.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 #from dataentry.models import Student
 from django.apps import apps
 import csv
+from django.db import DataError
 
 # Proposed command - python manage.py importdata file_path model_name
 
@@ -30,9 +31,19 @@ class Command(BaseCommand):
         if not model:
             raise CommandError(f'Model "{model_name}" not found in any app!')
 
+        # compare csv header with model's field names
+        # get all the field names of the model that we found
+        model_fields = [field.name for field in model._meta.fields if field.name != 'id']
+        print(model_fields)
+
         with open(file_path, 'r') as file:
-            #dict reader convert data into list dict format
+            # dict reader convert data into list dict format
             reader = csv.DictReader(file)
+            csv_header = reader.fieldnames
+            # compare csv header with model's field names
+            if csv_header != model_fields:
+                raise DataError(f"CSV file doesn't match with the {model_name} table fields.")
+
             for row in reader:
                 model.objects.create(**row)
         self.stdout.write(self.style.SUCCESS("Data imported from csv successfully!"))
