8660a19ef9edd073890543db45e6cbde6a3763da
shahbaj <rk2403476@gmil.com>
2024-02-23 14:07:27 +0530

    export data and send email with attachment

--- dataentry/views.py
+++ dataentry/views.py
@@ -3,7 +3,9 @@ from .utils import check_csv_errors, get_all_custom_models
 from uploads.models import Upload
 from django.conf import settings
 from django.contrib import messages
-from .tasks import import_data_task
+from .tasks import import_data_task,  export_data_task
+from django.core.management import call_command
+
 
 # Create your views here.
 
@@ -37,6 +39,25 @@ def import_data(request):
     else:
         custom_models = get_all_custom_models()
         context = {
-            'custom_models': custom_models
+            'custom_models': custom_models,
         }
     return render(request, 'dataentry/importdata.html', context)
+
+
+def export_data(request):
+    if request.method == "POST":
+        model_name = request.POST.get('model_name')
+
+        # export data task
+        export_data_task.delay(model_name)
+
+        # show the message to the user
+        messages.success(request, 'Your data is being exported, you will be notifield once it is done')
+        return redirect('export_data')
+    else:
+        custom_models = get_all_custom_models()
+        context = {
+            'custom_models': custom_models,
+        }
+    return render(request, 'dataentry/exportdata.html', context)
+
